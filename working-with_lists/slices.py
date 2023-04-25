d6c990e485ce29c0e605e927098f9ac24d412de1
nickaj1 <56038773+nickaj1@users.noreply.github.com>
2023-04-25 10:00:11 +0000

    Update slices.py

--- working-with_lists/slices.py
+++ working-with_lists/slices.py
@@ -1,11 +1,12 @@
+# Print first three item in list
 settings = ['airplane mode', 'wifi', 'privacy','focus', 'notification', 'general', 'wallpaper']
 print(settings[:3])
 print('The first three items in the list are\n')
 
-
+# Printing three middle items in list
 print(settings[2:5])
 print('The list of three middle items in the list\n')
 
-
+# Printing last three items in list
 print(settings[-3:])
 print('The list of the last three items in the list')
